85c83525e1ff3a7353a352752949b9cbb5801d72
albin.git <albin.git@gmail.com>
2017-06-20 16:16:31 +0530

    first commit

---
+++ QuintypeMusicPlayer/QuintypeMusicPlayer.h
@@ -0,0 +1,19 @@
+//
+//  QuintypeMusicPlayer.h
+//  QuintypeMusicPlayer
+//
+//  Created by Albin.git on 6/20/17.
+//  Copyright © 2017 Albin.git. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuintypeMusicPlayer.
+FOUNDATION_EXPORT double QuintypeMusicPlayerVersionNumber;
+
+//! Project version string for QuintypeMusicPlayer.
+FOUNDATION_EXPORT const unsigned char QuintypeMusicPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuintypeMusicPlayer/PublicHeader.h>
+
+
---
+++ QuintypeMusicPlayer/QuintypeMusicPlayer.swift
@@ -0,0 +1,19 @@
+//
+//  QuintypePlayer.swift
+//  QuintypeMusicPlayer
+//
+//  Created by Albin.git on 6/20/17.
+//  Copyright © 2017 Albin.git. All rights reserved.
+//
+
+import Foundation
+
+open class QuintypeMusicPlayer{
+    
+    
+    public func Work() -> String {
+        return "its working"
+    }
+    
+    
+}
